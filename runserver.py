876454880efd24de9b285438b8ef6a0e29647b75
Ardeshir <examples21@gmail.com>
2020-07-18 21:12:48 +0300

    changeport to 80

--- runserver.py
+++ runserver.py
@@ -4,4 +4,4 @@ from Cashboard.wsgi import application
 # documentation: https://docs.pylonsproject.org/projects/waitress/en/stable/api.html
 
 if __name__ == '__main__':
-    serve(application, host = 'localhost', port='8080')
+    serve(application, host = 'localhost', port='80')
